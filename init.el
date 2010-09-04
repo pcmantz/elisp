6c3bbf193f057926f7b97ec1b1cb100d0aadee4d
Paul C. Mantz <pcmantz@gmail.com>
2010-09-03 23:00:29 -0500

    init.el: added configuration for haskell-mode

--- init.el
+++ init.el
@@ -15,6 +15,14 @@
 
 ;; haskell-mode
 (require 'haskell-mode)
+(require 'inf-haskell)
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+
+(autoload 'turn-on-haskell-doc-mode "haskell-doc" nil t)
+
+(add-to-list 'auto-mode-alist '("\\.hs" . haskell-mode))
 
 ;; magit
 (require 'magit)
