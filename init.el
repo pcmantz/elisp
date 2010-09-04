8b98ff6c68a5000a54d198b8c277736541f528f5
Paul C. Mantz <pcmantz@gmail.com>
2010-09-03 22:58:30 -0500

    init.el: added eproject, recommended perl project configuration

--- init.el
+++ init.el
@@ -63,3 +63,29 @@
 ;; ibuffer-git (not yet customized)
 (require 'ibuffer-git)
 
+;; eproject
+(require 'eproject)
+(require 'eproject-extras)
+
+(define-project-type perl (generic)
+  (or (look-for "Makefile.PL") (look-for "Build.PL"))
+  :relevant-files ("\\.pm$" "\\.t$" "\\.pl$" "\\.PL$")
+  :irrelevant-files ("inc/" "blib/" "cover_db/")
+  :mxdeclare-project-p (lambda (root)
+                         (file-exists-p
+                          (concat root
+                                  ".mxdeclare_project")))
+  :file-name-map (lambda (root)
+                   (lambda (root file)
+                     (cond ((string-match "^lib/\\(.+\\)[.]pm$" file)
+                            (let ((m (match-string 1 file)))
+                              (while (string-match "/" m)
+                                (setf m (replace-match "::" nil nil m)))
+                              m))
+                           (t file))))
+  :main-file "Makefile.PL")
+
+(defun cperl-mxdeclare-project-p ()
+  "Determine if this project should use MooseX::Declare class definitions."
+  (ignore-errors
+    (eproject-attribute :is-mxdeclare-project)))
