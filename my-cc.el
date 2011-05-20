e136fae3dba3c1352dd5b72d8f53fa89ba650391
Paul C. Mantz <pcmantz@gmail.com>
2011-05-19 23:50:38 -0500

    * my-cc.el: added C/C++ mode. also, overdue.

---
+++ my-cc.el
@@ -0,0 +1,13 @@
+;; my-cc.el
+
+(defun my-c-mode-common-hook ()
+  (c-set-style "k&r")
+  (setq tab-width 4
+        c-basic-offset 4
+        c-electric-flag nil
+        c-tab-always-indent t))
+
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+
+(provide 'my-cc)
+;; end my-cc.el
