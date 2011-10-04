bb9f8ed2eb6900d5d29499864e2dd7ccae70ee73
Paul C. Mantz <pcmantz@gmail.com>
2011-10-03 23:27:28 -0500

    move org & org-agenda customization to where they belong

--- my-org.el
+++ my-org.el
@@ -4,15 +4,14 @@
 
 (require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
+(define-key global-map (kbd "C-c l") 'org-store-link)
+(define-key global-map (kbd "C-c a") 'org-agenda)
 (setq org-log-done t)
 
 (eval-after-load "org"
   '(progn
      (define-prefix-command 'org-todo-state-map)
-
-     (define-key org-mode-map "\C-cx" 'org-todo-state-map)
+     (define-key org-mode-map (kbd "C-c x") 'org-todo-state-map)
 
      (define-key org-todo-state-map "x"
        #'(lambda nil (interactive) (org-todo "CANCELLED")))
@@ -25,14 +24,50 @@
      (define-key org-todo-state-map "s"
        #'(lambda nil (interactive) (org-todo "STARTED")))
      (define-key org-todo-state-map "w"
-       #'(lambda nil (interactive) (org-todo "WAITING")))))
+       #'(lambda nil (interactive) (org-todo "WAITING")))
+
+     (setq
+      org-default-notes-file    "~/org/notes.org"
+      org-deadline-warning-days 14
+      org-reverse-note-order t
+      org-fast-tag-selection-single-key (quote expert)
+      org-remember-store-without-prompt t
+      org-remember-templates
+      '(("Todo" ?t "* TODO %?\n  %u" "~/org/todo.org"  "Tasks")
+        ("Note" ?n "* %u %?"         "~/org/notes.org" "Notes"))))) 
 
 (eval-after-load "org-agenda"
   '(progn
-     (define-key org-agenda-mode-map "\C-n" 'next-line)
-     (define-key org-agenda-keymap "\C-n" 'next-line)
-     (define-key org-agenda-mode-map "\C-p" 'previous-line)
-     (define-key org-agenda-keymap "\C-p" 'previous-line)))
+     (define-key org-agenda-mode-map (kbd "C-n") 'next-line)
+     (define-key org-agenda-keymap   (kbd "C-n") 'next-line)
+     (define-key org-agenda-mode-map (kbd "C-p") 'previous-line)
+     (define-key org-agenda-keymap   (kbd "C-p") 'previous-line)
+
+     (setq 
+      org-agenda-files          '("~/org/todo.org")
+      org-agenda-include-diary  t
+      org-agenda-ndays          14
+      org-agenda-show-all-dates t
+      org-agenda-skip-deadline-if-done  t
+      org-agenda-skip-scheduled-if-done t
+      org-agenda-start-on-weekday       nil
+      org-agenda-custom-commands
+      '(("d" todo "DELEGATED" nil)
+        ("c" todo "DONE|DEFERRED|CANCELLED" nil)
+        ("w" todo "WAITING" nil)
+        ("W" agenda "" ((org-agenda-ndays 21)))
+        ("A" agenda ""
+         ((org-agenda-skip-function
+           (lambda nil
+             (org-agenda-skip-entry-if (quote notregexp) "\\=.*\\[#A\\]")))
+          (org-agenda-ndays 1)
+          (org-agenda-overriding-header "Today's Priority #A tasks: ")))
+        ("u" alltodo ""
+         ((org-agenda-skip-function
+           (lambda nil
+             (org-agenda-skip-entry-if (quote scheduled) (quote deadline)
+                                       (quote regexp) "<[^>\n]+>")))
+          (org-agenda-overriding-header "Unscheduled TODO entries: ")))))))
 
 (provide 'my-org)
 ;; end my-org.el
--- my-remember.el
+++ my-remember.el
@@ -6,41 +6,9 @@
 (add-hook 'remember-mode-hook 'org-remember-apply-template)
 (define-key global-map [(control meta ?r)] 'remember)
 
-(custom-set-variables
- '(org-agenda-files (quote ("~/org/todo.org")))
- '(org-agenda-include-diary t)
- '(org-default-notes-file "~/org/notes.org")
- '(org-agenda-ndays 14)
- '(org-deadline-warning-days 14)
- '(org-agenda-show-all-dates t)
- '(org-agenda-skip-deadline-if-done t)
- '(org-agenda-skip-scheduled-if-done t)
- '(org-agenda-start-on-weekday nil)
- '(org-reverse-note-order t)
- '(org-fast-tag-selection-single-key (quote expert))
- '(org-agenda-custom-commands
-   (quote (("d" todo "DELEGATED" nil)
-           ("c" todo "DONE|DEFERRED|CANCELLED" nil)
-           ("w" todo "WAITING" nil)
-           ("W" agenda "" ((org-agenda-ndays 21)))
-           ("A" agenda ""
-            ((org-agenda-skip-function
-              (lambda nil
-                (org-agenda-skip-entry-if (quote notregexp) "\\=.*\\[#A\\]")))
-             (org-agenda-ndays 1)
-             (org-agenda-overriding-header "Today's Priority #A tasks: ")))
-           ("u" alltodo ""
-            ((org-agenda-skip-function
-              (lambda nil
-                (org-agenda-skip-entry-if (quote scheduled) (quote deadline)
-                                          (quote regexp) "<[^>\n]+>")))
-             (org-agenda-overriding-header "Unscheduled TODO entries: "))))))
- '(org-remember-store-without-prompt t)
- '(org-remember-templates
-   '(("Todo" ?t "* TODO %?\n  %u" "~/org/todo.org"  "Tasks")
-     ("Note" ?n "* %u %?"         "~/org/notes.org" "Notes")))
- '(remember-annotation-functions (quote (org-remember-annotation)))
- '(remember-handler-functions (quote (org-remember-handler))))
+(setq
+ remember-annotation-functions '(org-remember-annotation)
+ remember-handler-functions    '(org-remember-handler))
 
 (provide 'my-remember)
 ;; end my-remember.el
