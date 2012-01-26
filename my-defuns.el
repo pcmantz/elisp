5148000bfb0058e88f33bea4c827fd5475d2892b
Paul C. Mantz <pcmantz@gmail.com>
2012-01-26 11:46:40 -0600

    * my-defuns.el: add range function

--- my-defuns.el
+++ my-defuns.el
@@ -1,6 +1,8 @@
-;; my-defuns.el
+;; my-defuns.el -- helpful functions for every-day usage
 
-;; helpful functions for every day usage of perl
+;;
+;; Interactive functions
+;;
 
 (defun msg-buffer-filename () (interactive)
   (message (buffer-file-name)))
@@ -11,6 +13,22 @@
 (defun fullscreen () (interactive)
   (set-frame-parameter nil 'fullscreen
                        (if (frame-parameter nil 'fullscreen) nil 'fullboth)))
+
+;;
+;; elisp utils
+;;
+
+(defun range (first-arg &optional last-arg)
+  "Super-naïve implementation of a range function.  Update this
+do to this in reverse."
+  (let* ((start (if (null last-arg) 0         first-arg))
+         (end   (if (null last-arg) first-arg last-arg))
+         (iter start)
+         (range-list '()))
+    (while (<  iter end)
+      (add-to-list 'range-list iter 1)
+      (setq iter (+ iter 1)))
+    range-list))
 
 (provide 'my-defuns)
 ;; end my-defuns.el
