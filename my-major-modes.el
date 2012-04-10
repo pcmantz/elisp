3fbcc88fbf8bfd7c42338d26f0b20a4597bfa228
Paul C. Mantz <pcmantz@gmail.com>
2012-04-09 23:08:22 -0500

    * my-major-modes.el: reorganize and set up autoloading

--- my-major-modes.el
+++ my-major-modes.el
@@ -3,23 +3,21 @@
 ;; grab bag file for major modes that don't warrant their own config file
 ;; (yet)
 
-;; diff-mode
-(setq diff-switches "-u")
-(eval-after-load 'diff-mode
-  '(progn
-     (set-face-foreground 'diff-added "green4")
-     (set-face-foreground 'diff-removed "red3")))
-
-;; yaml-mode
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml$"  . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
-(eval-after-load 'yaml-mode
-  (setq yaml-indent-offset 'tab-width))
+;;
+;; programming language modes
+;;
 
 ;; sql-indent
 (eval-after-load "sql"
   '(load-library "sql-indent"))
+
+;;
+;; standalone applications
+;;
+
+;; browse-kill-ring
+(require 'browse-kill-ring)
+(browse-kill-ring-default-keybindings)
 
 ;; magit
 (require 'magit)
@@ -29,33 +27,52 @@
      (set-face-foreground 'magit-diff-add "green3")
      (set-face-foreground 'magit-diff-del "red3")
      (if (fboundp 'magit-completing-read-function) 
-         (setq magit-completing-read-function 'ido-completing-read))))
-
-;; note: may have to amend for unicode project names
-(add-to-list 'same-window-regexps "\\*magit: [[:ascii:]]\\*")
+         (setq magit-completing-read-function 'ido-completing-read))
+     (add-to-list 'same-window-regexps "\\*magit: [[:ascii:]]\\*")))
 
 ;; multi-term
 (require 'multi-term)
 (setq multi-term-program "/bin/bash")
 
-;; rnc-mode
-(add-to-list 'auto-mode-alist '("\\.rnc\\'" . rnc-mode))
-(autoload 'rnc-mode "rnc-mode"
-  "Major mode for editing Relax NG Compact schema files." t)
-(eval-after-load 'rnc-mode
-  '(progn (setq rnc-indent-level 'tab-width)
-          (setq rnc-mode-map (make-sparse-keymap))
-          (define-key rnc-mode-map "\C-c\C-c" 'comment-region)))
-
-
-;; csv-mode
-(add-to-list 'auto-mode-alist '("\\.[Cc][Ss][Vv]\\'" . csv-mode))
-(autoload 'csv-mode "csv-mode"
-  "Major mode for editing comma-separated value files." t)
-
 ;; browse-kill-ring
 (require 'browse-kill-ring)
 (browse-kill-ring-default-keybindings)
+
+;;
+;; markup modes
+;;
+
+;; diff-mode
+(setq diff-switches "-u")
+(eval-after-load 'diff-mode
+  '(progn
+     (set-face-foreground 'diff-added "green4")
+     (set-face-foreground 'diff-removed "red3")))
+
+;; yaml-mode
+(add-to-list 'auto-mode-alist '("\\.yml$"  . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
+(autoload 'yaml-mode "yaml-mode" "Major mode for editing YAML files." t)
+(eval-after-load 'yaml-mode
+  (setq yaml-indent-offset 'tab-width))
+
+;; rnc-mode
+(add-to-list 'auto-mode-alist '("\\.rnc\\'" . rnc-mode))
+(autoload 'rnc-mode "rnc-mode" "Major mode for editing Relax NG Compact schema files." t)
+(eval-after-load 'rnc-mode
+  '(progn
+     (setq rnc-indent-level 'tab-width)
+     (setq rnc-mode-map (make-sparse-keymap))
+     (define-key rnc-mode-map "\C-c\C-c" 'comment-region)))
+
+;; csv-mode
+(add-to-list 'auto-mode-alist '("\\.[Cc][Ss][Vv]\\'" . csv-mode))
+(autoload 'csv-mode "csv-mode" "Major mode for editing csv files." t)
+
+;; markdown-mode
+(add-to-list 'auto-mode-alist '("\\.md$"       . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+(autoload 'markdown-mode "markdown-mode" "Major mode for editing markdown files." t)
 
 (provide 'my-major-modes)
 ;; end my-major-modes.el
