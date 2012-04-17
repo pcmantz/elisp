c382387f4b655d701cc4a99d9f3f20829b3683c2
Paul C. Mantz <pcmantz@gmail.com>
2012-04-16 23:44:18 -0500

    * my-js.el: added line trimming and auto-loading

--- my-js.el
+++ my-js.el
@@ -1,5 +1,14 @@
 ;; my-js.el
 
+(add-to-list 'auto-mode-alist '("*\\.js$" . javascript-mode))
+
+(add-hook 'javascript-mode-hook
+          (lambda ()
+            (add-hook 'local-write-file-hooks
+                      '(lambda ()
+                         (save-excursion
+                           (delete-trailing-whitespace)))))
+          t)
 
 (provide 'my-js)
 ;; end my-js.el
