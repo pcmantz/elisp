65f1ce165c9d8f0d8371fc518bc744f568a8bc0d
Paul C. Mantz <pcmantz@gmail.com>
2012-06-11 01:11:45 -0500

    * my-lua.el: sane defaults

---
+++ my-lua.el
@@ -0,0 +1,17 @@
+;; my-lua.el
+
+(defun my-lua-defaults ()
+  (setq lua-indent-level tab-width))
+(setq my-lua-hook 'my-lua-defaults)
+
+(add-hook 'lua-mode-hook (lambda () (run-hooks 'my-lua-hook)) t)
+(add-hook 'lua-mode-hook
+          (lambda ()
+            (add-hook 'local-write-file-hooks
+                      '(lambda ()
+                         (save-excursion
+                           (delete-trailing-whitespace)))))
+          t)
+
+(provide 'my-lua)
+;; end my-lua.el
