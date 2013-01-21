6b717ec890e09efe1a1cb756fba73635012b1ef1
Paul C. Mantz <paul.mantz@cohesiveft.com>
2013-01-21 12:59:02 -0600

    <C-d C-d> to kill a term buffer window

--- my-major-modes.el
+++ my-major-modes.el
@@ -84,3 +84,19 @@
 
 (provide 'my-major-modes)
 ;; end my-major-modes.el
+
+
+;;
+;; Fundamental mode overrides
+;;
+
+(defun comint-delchar-or-eof-or-kill-buffer (arg)
+  (interactive "p")
+  (if (null (get-buffer-process (current-buffer)))
+      (kill-buffer)
+    (comint-delchar-or-maybe-eof arg)))
+
+(add-hook 'shell-mode-hook
+          (lambda ()
+            (define-key shell-mode-map
+              (kbd "C-d") 'comint-delchar-or-eof-or-kill-buffer)))
