c40a1477d20870898998934efb199ad13bdde106
Paul C. Mantz <paul.mantz@cohesiveft.com>
2013-02-01 10:55:11 -0600

    * my-major-modes.el: helpful Magit key for grokking diffs

--- my-major-modes.el
+++ my-major-modes.el
@@ -34,9 +34,27 @@
   '(progn
      (set-face-foreground 'magit-diff-add "green3")
      (set-face-foreground 'magit-diff-del "red3")
-     (if (fboundp 'magit-completing-read-function) 
+     (if (fboundp 'magit-completing-read-function)
          (setq magit-completing-read-function 'ido-completing-read))
      (add-to-list 'same-window-regexps "\\*magit: [[:ascii:]]\\*")))
+
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
 
 ;; multi-term
 (require 'multi-term)
