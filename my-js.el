946adc482b8a4c46b294b530090065733fffad15
Paul C. Mantz <pcmantz@gmail.com>
2014-08-06 09:24:07 -0500

    update javascript to current usage

--- my-js.el
+++ my-js.el
@@ -7,10 +7,16 @@
    (if (eq (cdr pair) 'javascript-mode)
        (setcdr pair 'js2-mode)))
  (append auto-mode-alist interpreter-mode-alist))
-
 (add-to-list 'auto-mode-alist '("*\\.js$" . js2-mode))
 
-(setq js2-basic-offset 2)
+(setq js2-basic-offset 4)
+
+(defun my-js-defaults ()
+  (setq js2-basic-offset 4)
+  (define-key js2-mode-map (kbd "C-M-\\") 'web-beautify-js))
+
+(setq my-js-hook 'my-js-defaults)
+(add-hook 'js2-mode-hook (lambda () (run-hooks 'my-js-hook)) t)
 
 (add-hook 'js2-mode-hook
           (lambda ()
