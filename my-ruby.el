3c1d2d6d484409c46d9fa0de47617c0d69aad754
Paul C. Mantz <paul.mantz@recsolu.com>
2015-09-22 14:57:15 -0500

    Use enh-ruby-mode and rubocop

--- my-ruby.el
+++ my-ruby.el
@@ -1,11 +1,8 @@
 ;; my-ruby.el
 
+;; enh-ruby-mode
+(require 'enh-ruby-mode)
 (defalias 'ruby-mode 'enh-ruby-mode)
-(mapc
- (lambda (pair)
-   (if (eq (cdr pair) 'ruby-mode)
-       (setcdr pair 'enh-ruby-mode)))
- (append auto-mode-alist interpreter-mode-alist))
 
 ;; (rx (and (or "Gem" "Rake" "Cap" "Vagrant") "file" (opt ".lock")))
 (add-to-list 'auto-mode-alist '("\\(?:Cap\\|Gem\\|Rake\\|Vagrant\\)file\\(?:\\.lock\\)?" . enh-ruby-mode))
@@ -13,11 +10,13 @@
 (add-to-list 'auto-mode-alist '("\\.rabl$" . enh-ruby-mode))
 
 (defun my-ruby-defaults ()
-  (setq
-   enh-ruby-deep-indent-paren nil))
+  (setq enh-ruby-deep-indent-paren nil))
 
 (setq my-ruby-hook 'my-ruby-defaults)
 (add-hook 'enh-ruby-mode-hook (lambda () (run-hooks 'my-ruby-hook)) t)
+
+(require 'rubocop)
+(add-hook 'enh-ruby-mode-hook 'rubocop-mode)
 
 (require 'ruby-tools)
 (add-hook 'enh-ruby-mode-hook 'ruby-tools-mode)
@@ -34,6 +33,12 @@
 (defun ruby-hash-arrows-to-keys-buffer ()
   (interactive)
   (ruby-hash-arrows-to-keys-region (point-min) (point-max)))
+
+(mapc
+ (lambda (pair)
+   (if (eq (cdr pair) 'ruby-mode)
+       (setcdr pair 'enh-ruby-mode)))
+ (append auto-mode-alist interpreter-mode-alist))
 
 (provide 'my-ruby)
 ;;end my-ruby.el
