8cc6c14d93de7fd66384476f6e2a2ad65c74cf7b
Paul C. Mantz <paul.mantz@yello.co>
2016-04-20 10:27:34 -0500

    handle loading of ruby stuff correctly

--- lisp/my-ruby.el
+++ lisp/my-ruby.el
@@ -4,36 +4,47 @@
 
 ;;; Code:
 
+(defun set-ruby-mode-to-enh-ruby-mode ()
+    "Replace `ruby-mode' with `enh-ruby-mode' in `auto-mode-alist' and `interpreter-mode-alist'."
+    (mapc
+     (lambda (pair)
+       (if (eq (cdr pair) 'ruby-mode)
+           (setcdr pair 'enh-ruby-mode)))
+     (append auto-mode-alist interpreter-mode-alist)))
+
 ;; enh-ruby-mode
 (use-package enh-ruby-mode
+  :commands enh-ruby-mode
   :mode
   ;; (rx (and (or "Gem" "Rake" "Cap" "Vagrant") "file" (opt ".lock")))
   (("\\(?:Cap\\|Gem\\|Rake\\|Vagrant\\)file\\(?:\\.lock\\)?" . enh-ruby-mode)
    ("\\.rake$" . enh-ruby-mode)
    ("\\.rabl$" . enh-ruby-mode))
-  :config
+  :init
   (progn
     (defalias 'ruby-mode 'enh-ruby-mode)
-    (mapc
-     (lambda (pair)
-       (if (eq (cdr pair) 'ruby-mode)
-           (setcdr pair 'enh-ruby-mode)))
-     (append auto-mode-alist interpreter-mode-alist))
+    (set-ruby-mode-to-enh-ruby-mode))
+  :config
+  (progn
     (defun my-ruby-defaults () (setq enh-ruby-deep-indent-paren nil))
     (defalias 'my-ruby-defaults 'my-ruby-hook)
-    (add-hook 'enh-ruby-mode-hook (lambda () (run-hooks 'my-ruby-hook)) t)))
+    (add-hook 'enh-ruby-mode-hook (lambda () (run-hooks 'my-ruby-hook)) t)
+    (add-hook 'enh-ruby-mode-hook 'rubocop-mode)
+    (add-hook 'enh-ruby-mode-hook 'robe-mode)
+    (add-hook 'enh-ruby-mode-hook 'ruby-tools-mode)))
 
 (use-package rubocop
-  :diminish rubocop-mode
-  :config (progn (add-hook 'enh-ruby-mode-hook 'rubocop-mode)))
-
+  :diminish rubocop-mode)
 (use-package ruby-tools
   :diminish ruby-tools)
-
-(use-package robe
-  :config (add-hook 'enh-ruby-mode-hook 'robe-mode))
-
+(use-package robe)
 (use-package chruby)
+
+(use-package projectile-rails
+  :init
+  (add-hook 'projectile-mode-hook 'projectile-rails-on)
+  :config
+  (set-ruby-mode-to-enh-ruby-mode))
 
 (defun ruby-hash-arrows-to-keys-region (beg end)
   "Replace symbol-arrow hash syntax with the newer 1.9 Javascript-like syntax."
