5e2430b0ae99f1f9308f703b02f17e86d039502c
Paul C. Mantz <pcmantz@gmail.com>
2016-05-07 19:19:54 -0500

    clean up js2-mode initialization and handling web-beautify-js

--- lisp/my-js.el
+++ lisp/my-js.el
@@ -4,6 +4,17 @@
 
 ;;; Code:
 
+(defun set-javascript-mode-to-js2-mode ()
+    "Replace `javascript-mode' with `js2-mode' in `auto-mode-alist' and `interpreter-mode-alist'."
+    (mapc
+     (lambda (pair)
+       (if (eq (cdr pair) 'javascript-mode)
+           (setcdr pair 'js2-mode)))
+     (append auto-mode-alist interpreter-mode-alist)))
+
+(set-javascript-mode-to-js2-mode)
+(defalias 'javascript-mode 'js2-mode)
+
 (use-package js2-mode
   :mode (("*\\.js$" . js2-mode))
   :config
@@ -12,18 +23,14 @@
       (setq js2-basic-offset 2)))
   :config
   (progn
-    (defalias 'javascript-mode 'js2-mode)
-    (mapc
-     (lambda (pair)
-       (if (eq (cdr pair) 'javascript-mode)
-           (setcdr pair 'js2-mode)))
-     (append auto-mode-alist interpreter-mode-alist))
+    (set-javascript-mode-to-js2-mode)
     (defvar my-js-hook 'my-js-defaults)
     (add-hook 'js2-mode-hook (lambda () (run-hooks 'my-js-hook)) t)))
 
 (use-package web-beautify
-  :config
-  (define-key js2-mode-map (kbd "C-M-\\") 'web-beautify-js))
+  :bind
+  (:js2-mode-map
+   ( "C-M-\\" . web-beautify-js)))
 
 (provide 'my-js)
 ;;; my-js.el ends here
