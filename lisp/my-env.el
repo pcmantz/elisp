3e0eb7978ad91412eeb8cf3967f26000e51b81b6
Paul C. Mantz <pmantz@cloudmineinc.com>
2017-09-30 20:58:30 -0500

    Enable Fira Code ligatures on MacOS

--- lisp/my-env.el
+++ lisp/my-env.el
@@ -69,6 +69,37 @@
      (ansi-color-for-comint-mode-on)))
 
 ;; graphical config
+
+(defun enable-fira-code-ligatures ()
+  "This function enables the ligatures found in the Fira Code font."
+  (let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
+         (35 . ".\\(?:###\\|##\\|_(\\|[#(?[_{]\\)")
+         (36 . ".\\(?:>\\)")
+         (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
+         (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
+         (42 . ".\\(?:\\(?:\\*\\*/\\)\\|\\(?:\\*[*/]\\)\\|[*/>]\\)")
+         (43 . ".\\(?:\\(?:\\+\\+\\)\\|[+>]\\)")
+         (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
+         (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
+         (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
+         (48 . ".\\(?:x[a-zA-Z]\\)")
+         (58 . ".\\(?:::\\|[:=]\\)")
+         (59 . ".\\(?:;;\\|;\\)")
+         (60 . ".\\(?:\\(?:!--\\)\\|\\(?:~~\\|->\\|\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[*$+~/<=>|-]\\)")
+         (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
+         (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
+         (63 . ".\\(?:\\(\\?\\?\\)\\|[:=?]\\)")
+         (91 . ".\\(?:]\\)")
+         (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
+         (94 . ".\\(?:=\\)")
+         (119 . ".\\(?:ww\\)")
+         (123 . ".\\(?:-\\)")
+         (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
+         (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)"))))
+    (dolist (char-regexp alist)
+      (set-char-table-range composition-function-table (car char-regexp)
+                `([,(cdr char-regexp) 0 font-shape-gstring])))))
+
 (when (display-graphic-p)
   (blink-cursor-mode -1)
   (mouse-wheel-mode t)
@@ -84,7 +115,8 @@
           select-enable-clipboard t
           interprogram-paste-function 'x-cut-buffer-or-selection-value))
         ((string= window-system "ns")  ;; Apple OS X
-         (set-face-attribute 'default nil :font "Inconsolata-11"))
+         (set-default-font "Fira Code")
+         (enable-fira-code-ligatures))
         ((string= window-system "w32") ;; MS-Windows
          (set-face-attribute 'default nil :font "Consolas-9"))
         (t nil)))
