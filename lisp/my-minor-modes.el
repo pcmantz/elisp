161ada2ceed421c702df1540186b642602b5ffa1
Paul C. Mantz <pmantz@cloudmineinc.com>
2017-09-30 20:59:32 -0500

    use eslint with local node config with flycheck

--- lisp/my-minor-modes.el
+++ lisp/my-minor-modes.el
@@ -25,6 +25,8 @@
 (use-package flycheck
   :config
   (progn
+    (setq-default flycheck-temp-prefix ".flycheck")
+
     (flycheck-define-checker ruby-reek
       "A Ruby smeel checker using reek
 See URL `https://github.com/troessner/reek'."
@@ -35,6 +37,24 @@ See URL `https://github.com/troessner/reek'."
       :modes (enh-ruby-mode ruby-mode)
       :next-checkers ((info . ruby-rubocop)))
     (add-to-list 'flycheck-checkers 'ruby-reek)
+
+    (setq-default flycheck-disabled-checkers
+                  (append flycheck-disabled-checkers '(javascript-jshint)))
+
+    (flycheck-add-mode 'javascript-eslint 'js2-mode)
+
+    ;; use local eslint from node_modules before global
+    ;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+    (defun my/use-eslint-from-node-modules ()
+      (let* ((root (locate-dominating-file
+                    (or (buffer-file-name) default-directory)
+                    "node_modules"))
+             (eslint (and root
+                          (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                            root))))
+        (when (and eslint (file-executable-p eslint))
+          (setq-local flycheck-javascript-eslint-executable eslint))))
+    (add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
 
     (global-flycheck-mode)))
 
