e757ad6dbff5d8e99e8c942e64397e12f5fa171b
Paul C. Mantz <pcmantz@gmail.com>
2019-03-14 16:36:07 -0500

    add coffee-mode configuration

---
+++ lisp/my-coffee.el
@@ -0,0 +1,10 @@
+;; my-coffee --- Coffescript configuration
+
+(use-package coffee-mode
+  :config
+  (progn
+    (custom-set-variables
+     '(coffee-tab-width 2))))
+
+(provide my-coffee)
+;;; my-coffee ends here
