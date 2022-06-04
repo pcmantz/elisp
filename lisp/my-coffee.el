c852c4384b5e9d58cdcc60f6112e1e105e0e5cf3
Paul C. Mantz <pcmantz@gmail.com>
2022-06-04 18:09:35 -0500

    update coffee-mode

--- lisp/my-coffee.el
+++ lisp/my-coffee.el
@@ -1,10 +1,8 @@
 ;; my-coffee --- Coffescript configuration
 
 (use-package coffee-mode
-  :config
-  (progn
-    (custom-set-variables
-     '(coffee-tab-width 2))))
+  :custom
+  (coffee-tab-width 2))
 
 (provide my-coffee)
 ;;; my-coffee ends here
