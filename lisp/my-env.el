676fbf02c4f911b29306d3841ddc4ccb2797c7a7
Paul C. Mantz <pcmantz@gmail.com>
2022-07-01 19:29:03 -0500

    clean up my-env

--- lisp/my-env.el
+++ lisp/my-env.el
@@ -35,10 +35,10 @@
 ;;
 (when (memq window-system '(mac ns))
   (use-package exec-path-from-shell
+    :custom
+    (exec-path-from-shell-check-startup-files nil)
     :config
-    (progn
-      (setq exec-path-from-shell-check-startup-files nil)
-      (exec-path-from-shell-initialize))))
+    (exec-path-from-shell-initialize)))
 
 ;;
 ;; backups
@@ -71,11 +71,11 @@
 ;; colors! the colors!
 (use-package font-lock
   :demand t
+  :custom
+  (font-lock-maximum-decoration t)
   :config
-  (progn
-     (global-font-lock-mode t)
-     (setq font-lock-maximum-decoration t)
-     (ansi-color-for-comint-mode-on)))
+  (global-font-lock-mode t)
+  (ansi-color-for-comint-mode-on))
 
 ;; graphical config
 
@@ -115,14 +115,12 @@
   (xterm-mouse-mode t)
   (use-package metalheart-theme
     :config
-    (progn
-      (load-theme 'metalheart)
-      (set-cursor-color "Purple")))
+    (load-theme 'metalheart)
+    (set-cursor-color "Purple"))
   (cond ((string= window-system "x")   ;; X11 window system
          (set-face-attribute 'default nil :font "Inconsolata-9")
-         (setq
-          select-enable-clipboard t
-          interprogram-paste-function 'x-cut-buffer-or-selection-value))
+         (setq select-enable-clipboard t
+               interprogram-paste-function 'x-cut-buffer-or-selection-value))
         ((string= window-system "ns")  ;; Apple OS X
          (set-face-attribute 'default nil :font "Inconsolata-11"))
         ((string= window-system "w32") ;; MS-Windows
@@ -150,10 +148,10 @@
 
 ;; dired
 (use-package dired-subtree
-  :config
-  (bind-keys :map dired-mode-map
-             ("i" . dired-subtree-insert)
-             (";" . dired-subtree-remove)))
+  :bind
+  (:map dired-mode-map
+    ("i" . dired-subtree-insert)
+    (";" . dired-subtree-remove)))
 
 ;; buffer listing
 (use-package ibuffer
@@ -163,10 +161,9 @@
   (ibuffer-always-show-last-buffer t)
   (ibuffer-view-ibuffer t)
   :config
-  (progn
-    (use-package ibuffer-vc
-      :config
-      (add-hook 'ibuffer-mode-hook 'ibuffer-vc-set-filter-groups-by-vc-root))))
+  (use-package ibuffer-vc
+    :config
+    (add-hook 'ibuffer-mode-hook 'ibuffer-vc-set-filter-groups-by-vc-root)))
 
 ;; frame config
 (line-number-mode t)
@@ -175,7 +172,7 @@
 ;; text display config
 (show-paren-mode t)
 (transient-mark-mode)
-(setq-default fill-column 78
+(setq-default fill-column 100
               tab-width 4
               indent-tabs-mode nil)
 
@@ -197,53 +194,51 @@
          ("<f13>" . helm-M-x)
          ("C-x C-f" . helm-find-files)
          ("C-x b". helm-mini)
-         ("M-y" . helm-show-kill-ring))
+          ("M-y" . helm-show-kill-ring))
+  :diminish helm-mode
   :config
-  (progn
-    (helm-mode 1)
-    (diminish 'helm-mode)
-    (defadvice helm-find-file (before make-directory-maybe (filename &optional wildcards) activate)
-      "Create parent directory if not exists while visiting file."
-      (unless (file-exists-p filename)
-        (let ((dir (file-name-directory filename)))
-          (unless (file-exists-p dir)
-            (make-directory dir)))))
-    (helm-autoresize-mode 1)))
+  (helm-mode 1)
+  (defadvice helm-find-file (before make-directory-maybe (filename &optional wildcards) activate)
+    "Create parent directory if not exists while visiting file."
+    (unless (file-exists-p filename)
+      (let ((dir (file-name-directory filename)))
+        (unless (file-exists-p dir)
+          (make-directory dir)))))
+  (helm-autoresize-mode 1))
 
 (use-package helm-config)
 
 ;; projectile: for managing projects
 (use-package projectile
-  :bind-keymap (("C-c p" . projectile-command-map)
-                ("s-p" . projectile-command-map))
+  :bind-keymap
+  (("C-c p" . projectile-command-map)
+   ("s-p" . projectile-command-map))
+  :custom
+    ;; HACK: latest implementation of projectile-mode-line is horribly un-performant and runs after
+    ;; each keypress, hammering the CPU
+    (projectile-mode-line . '(:eval (format " Pj<%s>" (projectile-project-name))))
   :config
-  (progn
-    (defun projectile-multi-term-in-root ()
-      "Invoke `multi-term' in the project's root."
-      (interactive)
-      (projectile-with-default-dir (projectile-project-root) (multi-term)))
+  (defun projectile-multi-term-in-root ()
+    "Invoke `multi-term' in the project's root."
+    (interactive)
+    (projectile-with-default-dir (projectile-project-root) (multi-term)))
 
     (define-key projectile-command-map (kbd "x m") 'projectile-multi-term-in-root)
-    (projectile-mode t)
-
-    ;; HACK: latest implementation of proectile-mode-line is horribly
-    ;; un-performant and runs after each keypress, hammering the CPU
-    (setq projectile-mode-line
-          '(:eval (format " Pj<%s>"
-                          (projectile-project-name))))))
+  (projectile-mode t))
 
 (use-package helm-projectile
-  :config (helm-projectile-on))
+  :config
+  (helm-projectile-on))
 
 ;; whitespace configuration
 ;; TODO: Make individual customizations for major modes
 (use-package whitespace
   :demand t
   :bind ("C-x W" . 'whitespace-mode)
+  :custom
+  ((whitespace-line-column 120))
   :config
-  (progn
-    (setq whitespace-line-column 120)
-    (add-hook 'before-save-hook 'whitespace-cleanup)))
+  (add-hook 'before-save-hook 'whitespace-cleanup))
 
 (use-package editorconfig
   :diminish editorconfig-mode
@@ -253,9 +248,8 @@
 (use-package keyfreq
   :demand t
   :config
-  (progn
-    (keyfreq-mode 1)
-    (keyfreq-autosave-mode 1)))
+  (keyfreq-mode 1)
+  (keyfreq-autosave-mode 1))
 
 (provide 'my-env)
 ;;; my-env.el ends here
