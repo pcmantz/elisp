c9411b75e59033f608aead5f0d9c5d1b431ca86c
Paul C. Mantz <pcmantz@mcpantz.org>
2024-04-01 18:34:44 -0500

    elixir mode

--- lisp/my-elixir.el
+++ lisp/my-elixir.el
@@ -6,9 +6,13 @@
 
 ;;; Code:
 
-(use-package elixir-mode)
+(use-package elixir-mode
+  :init
+  (add-to-list 'interpreter-mode-alist '("elixir" . elixir-mode)))
 
 (use-package elixir-yasnippets)
+
+(use-package heex-ts-mode)
 
 (provide 'my-elixir)
 
